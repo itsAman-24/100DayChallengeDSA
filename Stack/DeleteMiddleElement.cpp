adf5b9f6f93663648205e8c19361a38fc1fdf9ef
itsAman-24 <amanpan2410@gmail.com>
2024-01-14 21:24:14 +0530

    Added

---
+++ Stack/DeleteMiddleElement.cpp
@@ -0,0 +1,66 @@
+//WAP to delete the middle element from stack
+
+#include <bits/stdc++.h> 
+using namespace std;
+
+void solve(stack<int>&inputStack, int count ,int size) {
+   //Base case
+   if(count == size/2){
+      inputStack.pop();
+      return;
+   }
+
+   int num = inputStack.top();
+   inputStack.pop();
+
+   //Recursive calls
+   solve(inputStack,count+1,size);
+
+   inputStack.push(num);
+}
+
+void deleteMiddle(stack<int>&inputStack, int N){
+	
+   int count = 0;
+   solve(inputStack, count, N);
+   
+}
+
+int main() {
+    stack<int> myStack;
+
+    // Push some elements onto the stack
+    myStack.push(1);
+    myStack.push(2);
+    myStack.push(3);
+    myStack.push(4);
+    myStack.push(5);
+
+    // Display the original stack
+    cout << "Original Stack: ";
+    while (!myStack.empty()) {
+        cout << myStack.top() << " ";
+        myStack.pop();
+    }
+    cout << endl;
+
+    // Push the elements again
+    myStack.push(1);
+    myStack.push(2);
+    myStack.push(3);
+    myStack.push(4);
+    myStack.push(5);
+
+    // Delete the middle element
+    deleteMiddle(myStack, myStack.size());
+
+    // Display the modified stack
+    std::cout << "Stack after deleting middle element: ";
+    while (!myStack.empty()) {
+        cout << myStack.top() << " ";
+        myStack.pop();
+    }
+    cout << endl;
+
+    return 0;
+}
